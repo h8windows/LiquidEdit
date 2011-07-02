a261ed9d5006b99fad27a2a62411a45ae75cab16
Chris <h8windows@gmail.com>
2011-07-02 11:09:14 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Liquidedit::Application.config.secret_token = 'e102de957b462083d2c972eb3cf46d09ffb5e8a766b56af3d769051527ac2ef6aaae724544c66cd0655075e40a00441926ce4c444c79a1dd748ebf57c32c2315'
